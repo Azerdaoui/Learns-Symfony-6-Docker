f9594bb474ffc8f90969c5a7b890cb9654454e4e
AZERDAOUI Othmane <oazerdaoui@gear9.ma>
2022-12-16 17:29:39 +0100

    create new annotation route show_movie

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MovieController extends AbstractController
 {
-    #[Route('/movie', name: 'app_movie')]
+    #[Route('/movies', name: 'app_movies')]
     public function index(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/MovieController.php',
+        ]);
+    }
+
+    #[Route('/movies/{name}', name:'app_movies_show', methods:['GET', 'HEAD'])]
+    public function show(string $name): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to: ' . $name,
             'path' => 'src/Controller/MovieController.php',
         ]);
     }
