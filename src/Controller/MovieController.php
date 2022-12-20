ab5e9a537dd15bae1178e90c1f6eca82d6c80024
AZERDAOUI Othmane <oazerdaoui@gear9.ma>
2022-12-20 16:55:44 +0100

    return view in controller & install symf twig-bundle

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -5,11 +5,20 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class MovieController extends AbstractController
 {
-    #[Route('/movies', name: 'app_movies')]
-    public function index(): JsonResponse
+    #[Route('/movies/{title}', name: 'app_movies',  methods:['GET', 'HEAD'])]
+    public function index(string $title): Response
+    {
+        return $this->render('movies/index.html.twig', [
+            'title' => $title
+        ]);
+    }
+
+    #[Route('/json/movies', name: 'app_movies_json')]
+    public function indexJson(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
@@ -17,7 +26,7 @@ class MovieController extends AbstractController
         ]);
     }
 
-    #[Route('/movies/{name}', name:'app_movies_show', methods:['GET', 'HEAD'])]
+    #[Route('/json/movies/{name}', name:'app_movies_show_json', methods:['GET', 'HEAD'])]
     public function show(string $name): JsonResponse
     {
         return $this->json([
